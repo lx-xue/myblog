1d23d721d7b56c99aec5f8a0216d359d295c3b2e
liuxuexiaoxiao <18749817085@163.com>
2019-11-17 08:56:46 +0800

    index&add&eidit

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
 from blog import views
+
 urlpatterns = [
-    path('', views.index),
+    path('index/', views.index),
+    path('article/<article_id>/', views.article_page),
+    path('add/', views.add_page),
+    path('add/action/', views.add_action),
+    path('edit/<article_id>/', views.edit_page),
+    path('edit_action/', views.edit_action),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,53 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . import models
 
+
+# 文章首页
 def index(request):
-    return render(request, 'blog/index.html')
+    articles = models.Article.objects.all()
+    return render(request, 'blog/index.html', {'articles': articles})
+
+
+# 文章详情
+def article_page(request, article_id):
+    article = models.Article.objects.get(pk=article_id)
+    return render(request, 'blog/article_page.html', {'article': article})
+
+
+# 添加页面
+def add_page(request):
+    return render(request, 'blog/add_page.html')
+
+
+# 提交添加
+def add_action(request):
+    title = request.POST['title']
+    content = request.POST['content']
+    models.Article.objects.create(title=title, content=content)
+    articles = models.Article.objects.all()
+    return render(request, 'blog/index.html', {'articles': articles})
+
+
+# 编辑页面
+def edit_page(request, article_id):
+    article = models.Article.objects.get(pk=article_id)
+    return render(request, 'blog/edit_page.html', {'article': article})
+
+
+# 提交编辑
+def edit_action(request):
+    # articles = models.Article.objects.all()
+    # return render(request, 'blog/index.html', {'articles': articles})
+    id = request.POST['id']
+    title = request.POST['title']
+    content = request.POST['content']
+    if id == '0':
+        models.Article.objects.create(title=title, content=content)
+        articles = models.Article.objects.all()
+        return render(request, 'blog/index.html', {'articles': articles})
+    article = models.Article.objects.get(pk=id)
+    article.title = title
+    article.content = content
+    article.save()
+    return render(request, 'blog/article_page.html', {'article': article})
